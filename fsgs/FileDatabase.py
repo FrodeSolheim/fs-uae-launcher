f0a5bd75934c4c91c295a3b20950ccde30ff3a9f
Frode Solheim <frode@fs-uae.net>
2016-03-30 00:26:34 +0200

    force file database reset, database cleanup

--- fsgs/FileDatabase.py
+++ fsgs/FileDatabase.py
@@ -1,19 +1,17 @@
 import os
+import sqlite3
 import time
 from binascii import unhexlify
-import sqlite3
-# FIXME: remove dependency on launcher, have the Launcher tell this class
-# the path instead
-from fsgs.FSGSDirectories import FSGSDirectories
-from .BaseDatabase import BaseDatabase
 
+from fsgs.BaseDatabase import BaseDatabase
+from fsgs.FSGSDirectories import FSGSDirectories
 
 SENTINEL = "fae7671d-e232-4b71-b179-b3cd45995f92"
-VERSION = 2
+VERSION = 3
+RESET_VERSION = 3
 
 
 class File(dict):
-    
     def __nonzero__(self):
         # noinspection PyTypeChecker
         return bool(self["path"])
@@ -24,7 +22,6 @@ class File(dict):
 
 
 class FileDatabase(BaseDatabase):
-
     def __init__(self, sentinel):
         BaseDatabase.__init__(self, sentinel)
         self.last_file_insert = None
@@ -34,9 +31,11 @@ class FileDatabase(BaseDatabase):
     def get_path(cls):
         return os.path.join(FSGSDirectories.databases_dir(), "Files.sqlite")
 
-    @classmethod
-    def get_version(cls):
+    def get_version(self):
         return VERSION
+
+    def get_reset_version(self):
+        return RESET_VERSION
 
     @classmethod
     def instance(cls, new=False):
@@ -59,39 +58,17 @@ class FileDatabase(BaseDatabase):
         path = self.encode_path(path)
         if path.endswith("/"):
             path = path[:-1]
-        a_path = path + "\u002f"  # forward slash
-        b_path = path + "\u0030"  # one more than forward slash
+        a_path = path + "\u002f"  # Forward slash
+        b_path = path + "\u0030"  # Forward slash + 1
         cursor = self.internal_cursor()
         cursor.execute(
             "SELECT id FROM file WHERE path >= ? AND path < ?",
             (a_path, b_path))
         return set([row[0] for row in cursor.fetchall()])
 
-    # def get_child_ids(self, id=id):
-    #     cursor = self.internal_cursor()
-    #     cursor.execute("SELECT id FROM file WHERE parent = ?", (id,))
-    #     return set([row[0] for row in cursor.fetchall()])
-
-    # def get_file_hierarchy(self, path):
-    #     self.init()
-    #     cursor = self.create_cursor()
-    #     #query = "SELECT id, path FROM configuration WHERE path like ?"
-    #     #args = ["$BASE/Configurations/%"]
-    #     #self._cursor.execute(query, args)
-    #     a_path = path + "\u002f" # forward slash
-    #     b_path = path + "\u0030" # one more than forward slash
-    #     query = "SELECT id, path FROM file WHERE " \
-    #             "path >= ? AND path < ?"
-    #     cursor.execute(query, (a_path, b_path))
-    #     result = {}
-    #     for row in cursor:
-    #         result[decode_path(row[1])] = row[0]
-    #     return result
-
     def encode_path(self, path):
-        # this only works if both path and 
-        # FSGSDirectories.base_dir (etc) have been
-        # normalized with get_real_case.
+        # This only works if both path and FSGSDirectories.base_dir (etc) have
+        # been normalized with get_real_case.
         path = path.replace("\\", "/")
         base_dir = FSGSDirectories.get_base_dir()
         if path.startswith(base_dir):
@@ -100,18 +77,11 @@ class FileDatabase(BaseDatabase):
                 path = path[1:]
             path = "$/" + path
         return path
-        # parts = path.split("/")
-        # return "{0}{1}".format(len(parts), path)
 
     def decode_path(self, path):
-        # path = path[1:]
         if not path or path[0] != "$":
             return path
         base_dir = FSGSDirectories.get_base_dir() + "/"
-        # FIXME: $base_dir is deprecated
-        # path = path.replace("$base_dir/", base_dir)
-        # path = path.replace("$BASE/", base_dir)
-        # path = path.replace("$/", base_dir)
         if path.startswith("$/"):
             path = base_dir + path[2:]
         return path
@@ -119,11 +89,9 @@ class FileDatabase(BaseDatabase):
     def find_local_roms(self):
         if not self.connection:
             self.init()
-
         a = "$/Kickstarts/"
         b = "$/Kickstarts" + "\u0030"  # one more than forward slash
         query = "SELECT id, path FROM file WHERE path >= ? AND path < ?"
-        # args = ["$BASE/Kickstarts/%"]
         cursor = self.internal_cursor()
         cursor.execute(query, (a, b))
         result = {}
@@ -140,18 +108,13 @@ class FileDatabase(BaseDatabase):
             path = self.encode_path(path)
             cursor.execute("SELECT id FROM file WHERE path = ?", (path,))
             for row in cursor:
-                delete_ids.append(row[0])            
+                delete_ids.append(row[0])
         for id in delete_ids:
             cursor.execute(
                 "DELETE FROM file WHERE id = ? OR parent = ?", (id, id))
         self.last_file_delete = int(time.time())
 
     def check_sha1(self, sha1):
-        # if not hasattr(self, "check_sha1_cursor"):
-        #     #self.check_sha1_cursor = cursor()
-        #
-        #     self.init()
-        #     self.check_sha1_cursor = self.create_cursor()
         cursor = self.internal_cursor()
         cursor.execute(
             "SELECT count(*) FROM file WHERE sha1 = ?", (
@@ -167,6 +130,7 @@ class FileDatabase(BaseDatabase):
                 "FROM file WHERE sha1 = ? LIMIT 1",
                 (sqlite3.Binary(sha1),))
         elif name:
+            # noinspection SpellCheckingInspection
             cursor.execute(
                 "SELECT id, path, sha1, mtime, size, parent "
                 "FROM file WHERE name = ? COLLATE NOCASE LIMIT 1",
@@ -187,21 +151,17 @@ class FileDatabase(BaseDatabase):
             else:
                 path = row[1]
             path = self.decode_path(path)
-            # if result is not None:
             result["id"] = row[0]
             result["path"] = path
             result["sha1"] = row[2]
             result["mtime"] = row[3]
             result["size"] = row[4]
-            # return result
         else:
-            # if result is not None:
             result["id"] = None
             result["path"] = None
             result["sha1"] = None
             result["mtime"] = None
             result["size"] = None
-            # return None
         return result
 
     def find_files(self, ext=None):
@@ -244,18 +204,6 @@ class FileDatabase(BaseDatabase):
         self.last_file_insert = int(time.time())
         return cursor.lastrowid
 
-    # def ensure_dir(self, path):
-    #     #if not path.endswith("/"):
-    #     #    path += "/"
-    #     assert path.endswith("/")
-    #     cursor = self.internal_cursor()
-    #     cursor.execute("SELECT id FROM file WHERE name = ?", (path,))
-    #     row = cursor.fetchone()
-    #     if row is not None:
-    #         return row[0]
-    #     cursor.execute("INSERT INTO file (name) VALUES (?)", (path,))
-    #     return cursor.lastrowid
-
     def get_last_event_stamps(self):
         cursor = self.internal_cursor()
         cursor.execute(
@@ -287,7 +235,7 @@ class FileDatabase(BaseDatabase):
         cursor = self.internal_cursor()
         cursor.execute("DELETE FROM file")
 
-    def update_database_to_version_1(self):
+    def update_database_to_version_3(self):
         cursor = self.internal_cursor()
         try:
             cursor.execute("SELECT count(*) FROM file")
@@ -302,44 +250,9 @@ class FileDatabase(BaseDatabase):
                     )""")
             cursor.execute("CREATE INDEX file_sha1 ON file(sha1)")
             cursor.execute("CREATE INDEX file_path ON file(path)")
+            cursor.execute("CREATE INDEX file_parent ON file(parent)")
 
         cursor.execute(
             "ALTER TABLE metadata ADD COLUMN last_file_insert INTEGER")
         cursor.execute(
             "ALTER TABLE metadata ADD COLUMN last_file_delete INTEGER")
-
-    def update_database_to_version_2(self):
-        cursor = self.internal_cursor()
-        cursor.execute("CREATE INDEX file_parent ON file(parent)")
-
-    # def update_database_to_version_1(self):
-    #     cursor = self.create_cursor()
-    #     try:
-    #         cursor.execute("SELECT count(*) FROM file")
-    #     except sqlite3.OperationalError:
-    #         cursor.execute("""CREATE TABLE file (
-    # id integer primary key,
-    # path text,
-    # name text,
-    # sha1 text,
-    # size integer,
-    # mtime integer
-    # )""")
-    #         cursor.execute("CREATE INDEX file_sha1 ON file(sha1)")
-    #         cursor.execute("CREATE INDEX file_path ON file(path)")
-    #         cursor.execute("CREATE INDEX file_name ON file("
-    #                        "name collate nocase)")
-    #
-    # def update_database_to_version_2(self):
-    #     cursor = self.create_cursor()
-    #     cursor.execute("SELECT id, sha1 FROM file WHERE sha1 IS NOT NULL")
-    #     for row in cursor.fetchall():
-    #         id, sha1 = row
-    #         sha1 = unhexlify(sha1)
-    #         cursor.execute("UPDATE file SET sha1 = ? WHERE id = ?",
-    #                 (sqlite3.Binary(sha1), id))
-    #
-    # def update_database_to_version_3(self):
-    #     cursor = self.create_cursor()
-    #     cursor.execute("ALTER TABLE metadata ADD COLUMN last_file_insert INT")
-    #     cursor.execute("ALTER TABLE metadata ADD COLUMN last_file_delete INT")
