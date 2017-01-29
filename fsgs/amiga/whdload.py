4b776b29004b296906486bb8c9678eba906bd28c
Frode Solheim <frode@fs-uae.net>
2017-01-29 17:17:49 +0100

    Updated whdload module

--- fsgs/amiga/whdload.py
+++ fsgs/amiga/whdload.py
@@ -1,53 +1,47 @@
+import logging
 import os
-import traceback
+from typing import List, Dict, DefaultDict, BinaryIO
 
 from fsbc import settings
 from fsgs.Archive import Archive
 from fsgs.option import Option
 
 
-def should_disable_drive_click():
-    """Return True when drive clicks should be disabled when generating a
-    WHDLoad configuration."""
+def should_disable_drive_click() -> bool:
+    """Check if drive clicks should be disabled when generating a config."""
     return True
 
 
-def create_prefs_file(config, path):
-    # FIXME: require config object returning empty string on unset keys?
-    # print(repr(config.get("__invalid__")))
-    # assert config.get("__invalid__") == ""
-    prefs = default_whdload_prefs
+def create_prefs_file(config: DefaultDict[str, str], path: str) -> bool:
+    """Write a WHDLoad.prefs respecting options from config.
 
-    if config.get(Option.NETPLAY_GAME, ""):
+    Returns true if the prefs file was created.
+    """
+    if config[Option.NETPLAY_GAME]:
         # The options below are commonly retrieved from settings, not
         # config, and settings are not synced in net play, so we use
         # default settings.
-        print("WHDLoad defaults only in net play mode")
-    else:
-        splash_delay = config.get(Option.WHDLOAD_SPLASH_DELAY, "")
-        if splash_delay:
-            prefs = prefs.replace(
-                ";SplashDelay=0", "SplashDelay={}".format(
-                    int(splash_delay)))
-        quit_key = config.get(Option.WHDLOAD_QUIT_KEY, "")
-        if quit_key:
-            prefs = prefs.replace(
-                ";QuitKey=$45", "QuitKey={}".format(
-                    quit_key))
-        # WHDLoad.prefs does not like the PRELOAD option, adding to
-        # executable arguments instead.
-        # if config.get(Option.WHDLOAD_PRELOAD, "") != "0":
-        #     prefs += "\nPreload\n"
-
-    print(prefs)
+        logging.info("[WHDLOAD] No WHDLoad overrides in net play mode")
+        return False
+    prefs = default_whdload_prefs
+    splash_delay = config[Option.WHDLOAD_SPLASH_DELAY]
+    if splash_delay:
+        prefs = prefs.replace(
+            ";SplashDelay=0", "SplashDelay={}".format(
+                int(splash_delay)))
+    quit_key = config[Option.WHDLOAD_QUIT_KEY]
+    if quit_key:
+        prefs = prefs.replace(
+            ";QuitKey=$45", "QuitKey={}".format(
+                quit_key))
     # Make sure the data is CRLF line terminated.
-    prefs = prefs.replace("\r\n", "\n")
-    prefs = prefs.replace("\n", "\r\n")
+    prefs = prefs.replace("\r\n", "\n").replace("\n", "\r\n")
     with open(path, "wb") as f:
         f.write(prefs.encode("ISO-8859-1"))
+    return True
 
 
-def override_config(config):
+def override_config(config: DefaultDict[str, str]):
     if should_disable_drive_click():
         config[Option.FLOPPY_DRIVE_VOLUME_EMPTY] = "0"
     model = settings.get(Option.WHDLOAD_MODEL)
@@ -65,19 +59,17 @@ def override_config(config):
         config[Option.FAST_MEMORY] = "8192"
 
 
-def read_whdload_args_from_info_stream(stream):
-    return read_whdload_args_from_info_data(stream.read())
-
-
-def read_whdload_args_from_info_data(data):
-    print("[WHDLOAD] Read WHDLoad args from info data")
+def read_whdload_args_from_info_data(data: bytes) -> List[str]:
+    logging.debug("[WHDLOAD] Read WHDLoad args from info data")
     index = data.lower().find(b"slave=") - 1
+    if not index:
+        return []
     args = []
     parts = data[index:].split(b"\x00\x00\x00\x00")
     for part in parts[:]:
         if len(part) > 2:
             length = part[0]
-            # print(length)
+            # logging.debug("%d", length)
             arg = part[1:1 + length - 1]
             if b"***" in arg:
                 break
@@ -85,7 +77,11 @@ def read_whdload_args_from_info_data(data):
     return args
 
 
-def strip_whdload_slave_prefix(whdload_args):
+def read_whdload_args_from_info_stream(stream: BinaryIO) -> List[str]:
+    return read_whdload_args_from_info_data(stream.read())
+
+
+def strip_whdload_slave_prefix(whdload_args: List[str]) -> List[str]:
     result = []
     for i, arg in enumerate(whdload_args):
         arg = arg.split(";")[0]
@@ -96,52 +92,72 @@ def strip_whdload_slave_prefix(whdload_args):
     return result
 
 
-def calculate_whdload_args(archive_path):
+def fix_whdload_args(args: List[str]) -> str:
+    """Converts args list to string and corrects args for a few icons.
+
+    The corrections remove binary garbage from the end of the arguments list
+    (either there's a problem with the icons, or there's a bug in the
+    .info "parser", the latter being the most likely.
+    """
+    args_str = " ".join(args)
+    if args_str.startswith("KingsQuestEnhanced.Slave PRELOAD  /\x8c"):
+        args_str = "KingsQuestEnhanced.Slave PRELOAD"
+    elif args_str.startswith("PsychoKillerCDTV.Slave PRELOAD 3\x11"):
+        args_str = "PsychoKillerCDTV.Slave PRELOAD"
+    elif args_str.startswith("QuestForGlory.Slave PRELOAD \x00"):
+        args_str = "QuestForGlory.Slave PRELOAD"
+    elif args_str.startswith("Lorna.Slave PRELOAD \n"):
+        args_str = "Lorna.Slave PRELOAD"
+    return args_str
+
+
+def calculate_whdload_args(archive_path: str) -> str:
     """
     This function, as it is currently written, only works if there
     is an .info with the same name as the .slave file. In theory, they
     could be different since the .info file contains a slave=... tool type.
     """
     archive = Archive(archive_path)
-    slave = ""
-    args = []
+    slave_args = {}
     lower_to_name = {}
     for path in archive.list_files():
         lower_to_name[path.lower()] = path
     for path in lower_to_name.values():
         name = os.path.basename(path)
         name_lower = name.lower()
-        if name_lower.endswith(".slave"):
-            if slave:
-                print("[WHDLOAD] Already found one slave, don't know "
-                      "which one to choose")
-                return ""
-            slave = name
+        if name_lower.endswith(".info"):
             try:
-                info_path = lower_to_name[
-                    os.path.splitext(path)[0].lower() + ".info"]
-            except KeyError:
-                # No corresponding info file found.
-                pass
+                args = read_whdload_args_from_info_stream(
+                    archive.open(path))
+                args = strip_whdload_slave_prefix(args)
+            except Exception as e:
+                logging.warning(
+                    "[WHDLOAD] WARNING: Error reading args: %s", repr(e))
             else:
-                try:
-                    args = read_whdload_args_from_info_stream(
-                        archive.open(info_path))
-                    args = strip_whdload_slave_prefix(args)
-                except Exception as e:
-                    print("[WHDLOAD] WARNING: Error reading args:", repr(e))
+                if args:
+                    archive_name = path.rsplit("#/", 1)[1]
+                    logging.debug("[WHDLOAD] %s => %s".format(
+                        archive_name, " ".join(args)))
+                    slave_args[archive_name] = args
         elif name_lower == "startup-sequence":
-            print("[WHDLOAD] Found startup-sequence, assuming non-WHDLoad "
-                  "archive")
+            logging.info("[WHDLOAD] Found Startup-Sequence, assuming "
+                         "non-WHDLoad archive")
             return ""
-    if args:
-        return " ".join(args)
-    else:
-        return slave
+    if len(slave_args) == 0:
+        return ""
+    if len(slave_args) > 1:
+        logging.debug("[WHDLOAD] Multiple WHDLoad icons found")
+        for icon, args in slave_args.items():
+            if icon.lower() in primary_icons:
+                logging.debug("[WHDLOAD] Choosing %s as primary icon", icon)
+                return fix_whdload_args(args)
+        raise Exception("Multiple slaves found")
+    return fix_whdload_args(slave_args.popitem()[1])
 
 
-def generate_config_for_archive(path, model_config=True):
-    print("[WHDLOAD] Generate config for archive", path)
+def generate_config_for_archive(path: str, model_config: bool=True) \
+        -> Dict[str, str]:
+    logging.debug("[WHDLOAD] Generate config for archive %s", path)
     config = {}
     whdload_args = ""
     dummy, ext = os.path.splitext(path)
@@ -149,8 +165,8 @@ def generate_config_for_archive(path, model_config=True):
         try:
             whdload_args = calculate_whdload_args(path)
         except Exception:
-            traceback.print_exc()
-    config["x_whdload_args"] = whdload_args
+            logging.exception("[WHDLOAD] Error while calculating WHDLoad args")
+    config[Option.X_WHDLOAD_ARGS] = whdload_args
     if whdload_args and model_config:
         config[Option.AMIGA_MODEL] = "A1200"
         config[Option.FAST_MEMORY] = "8192"
@@ -296,7 +312,7 @@ default_whdload_prefs = """
 ;WriteDelay=150
 """
 # noinspection SpellCheckingInspection
-primary_icons = set([
+primary_icons = {_.lower() for _ in [
     "3DPool/3DPool.info",
     "BartVsSpaceMutants2Disk/BartVsSpaceMutants2Disk.info",
     "BattleIsle/BattleIsle.info",
@@ -349,4 +365,4 @@ primary_icons = set([
     "Virocop/Virocop2Meg.info",
     "WalkerCrunched/WalkerCrunched.info",
     "WalkerDecrunched/WalkerDecrunched.info",
-])
+]}
