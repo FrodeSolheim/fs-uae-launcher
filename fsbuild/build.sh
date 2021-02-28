275b3789feb5ef6136ebf1050c03fe6838b8713c
Frode Solheim <frode@solheim.dev>
2021-02-28 15:23:49 +0100

    Try Windows build

--- fsbuild/build.sh
+++ fsbuild/build.sh
@@ -6,12 +6,17 @@ make
 BUILDDIR=fsbuild/_build
 
 rm -Rf $BUILDDIR/pyinstaller
-
-if [ "$SYSTEM_OS" = "macOS" ]; then
+if [ "$SYSTEM_OS" = "Windows" ]; then
+pyinstaller \
+	--windowed \
+	--specpath pyinstaller \
+	--distpath $BUILDDIR/pyinstaller \
+	$PACKAGE_NAME
+elif [ "$SYSTEM_OS" = "macOS" ]; then
 false
 else
 pyinstaller \
---specpath pyinstaller \
---distpath $BUILDDIR/pyinstaller \
-$PACKAGE_NAME
+	--specpath pyinstaller \
+	--distpath $BUILDDIR/pyinstaller \
+	$PACKAGE_NAME
 fi
--- src/icons/glowicon.sh
+++ src/icons/glowicon.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 SCRIPTDIR=`dirname "$0"`
+if [ "$OS" = "Windows_NT" ]; then
+export PYTHONPATH="$SCRIPTDIR/../..;$PYTHONPATH"
+else
 export PYTHONPATH="$SCRIPTDIR/../..:$PYTHONPATH"
+fi
 python3 -m launcher.extra.glowicon "$@"
