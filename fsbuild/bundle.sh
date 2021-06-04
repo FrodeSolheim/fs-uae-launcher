10c81c2dba74c57fbbfeb8d47a8db01fd267e65b
Frode Solheim <frode@solheim.dev>
2021-06-04 21:27:55 +0200

    Correct bundle.sh for stable branch

--- fsbuild/bundle.sh
+++ fsbuild/bundle.sh
@@ -41,23 +41,23 @@ cat > $PLUGIN_BINDIR/"$PACKAGE_DISPLAY_NAME.app"/Contents/Info.plist <<EOF
 </dict>
 </plist>
 EOF
+mkdir "$PLUGIN_BINDIR/$PACKAGE_DISPLAY_NAME.app/Contents/Resources/Locale"
+cp -pPR share/locale/* "$PLUGIN_BINDIR/$PACKAGE_DISPLAY_NAME.app/Contents/Resources/Locale/"
 else
 cp -a fsbuild/_build/pyinstaller/$PACKAGE_NAME/* $PLUGIN_BINDIR/
 fi
 
 echo "Copying data files..."
-mkdir -p "$PLUGIN_DATADIR"
-cp -a data/* "$PLUGIN_DATADIR/"
 mkdir -p "$PLUGIN_DATADIR/arcade"
-cp -a ./arcade/data "$PLUGIN_DATADIR/arcade/"
+cp -a ./arcade/res "$PLUGIN_DATADIR/arcade/"
 mkdir -p "$PLUGIN_DATADIR/launcher"
-cp -a ./launcher/data "$PLUGIN_DATADIR/launcher/"
-mkdir -p "$PLUGIN_DATADIR/fsgamesys"
-cp -a ./fsgamesys/data "$PLUGIN_DATADIR/fsgamesys/"
+cp -a ./launcher/res "$PLUGIN_DATADIR/launcher/"
+mkdir -p "$PLUGIN_DATADIR/fsgs"
+cp -a ./fsgs/res "$PLUGIN_DATADIR/fsgs/"
 mkdir -p "$PLUGIN_DATADIR/fsui"
-cp -a ./fsui/data "$PLUGIN_DATADIR/fsui/"
+cp -a ./fsui/res "$PLUGIN_DATADIR/fsui/"
 mkdir -p "$PLUGIN_DATADIR/workspace"
-cp -a ./workspace/data "$PLUGIN_DATADIR/workspace/"
+cp -a ./workspace/res "$PLUGIN_DATADIR/workspace/"
 
 PLUGIN_SKIP_APPIFY=1
 PLUGIN_SKIP_STANDALONE=1
