4b789e3c543c69d707ec2f3ff1cb3f14fbb0aaf7
Frode Solheim <frode@solheim.dev>
2025-06-04 12:33:03 +0200

    Removed (now unused) fsbuild dir

--- fsbuild/version.post.sh
+++
@@ -1,5 +0,0 @@
-. ./PACKAGE.FS
-
-echo "VERSION = \"$PACKAGE_VERSION\"" > launcher/version.py
-echo "PACKAGER = \"$PACKAGE_PACKAGER\"" >> launcher/version.py
-echo "COMMIT = \"$PACKAGE_COMMIT\"" >> launcher/version.py
